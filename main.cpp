a0bd972b3535688b0f179c72e2287ef1bfa45253
watanabe-akihiro <k022g0118@g.neec.ac.jp>
2024-02-13 17:22:58 +0900

    最新

--- main.cpp
+++ main.cpp
@@ -1,20 +1,48 @@
 #include<stdio.h>
-#include "Box.h"
+#include <list>
+#include <string>
+#include <iostream>
+using namespace std;
 
 int main() {
-	Box<int, int>n1(10, 5);
-	Box<float, float>n2(3.5f, 2.4f);
-	Box<double, double>n3(7.5, 9.9);
-	Box<int, float>n4(10, 5.0f);
-	Box<int, double>n5(3, 4.0);
-	Box<float, double>n6(15.0f, 18.0);
 
-	printf("%d\n", n1.Min());
-	printf("%f\n", n2.Min());
-	printf("%lf\n", n3.Min());
-	printf("%d\n", n4.Min());
-	printf("%d\n", n5.Min());
-	printf("%f\n", n6.Min());
+	list<const char*> lst{
+		"Tokyo", "Kand", "Akihabara", "Okachimachi", "Ueno", "Uguisudani", "Nippori",
+			"Tabata", "Komagome", "Sugamo", "Otsuka", "Ikebukuro", "Mejiro", "Takadanobaba", "Shin-Okubo",
+			"Shinjuku", "Yoyogi", "Harajuku", "Shibuya", "Ebisu", "Meguro", "Gotanda", "Osaki", "Shinagawa",
+			"Tamachi", "Hamamatutsucho", "Shimbashi", "Yurakucho",
+	};
+
+	//1970
+	printf("1970”N\n");
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+		std::cout << *itr << "\n";
+	}
+	printf("\n");
+	//2019
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+		if (*itr == "Tabata") {
+			itr = lst.insert(itr, "Nishi-Nippori");
+			++itr;
+		}
+	}
+	printf("2019”N\n");
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+		std::cout << *itr << "\n";
+	}
+	printf("\n");
+	//2022
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+		if (*itr == "Tamachi") {
+			itr = lst.insert(itr, "Takanawa Gateway");
+			++itr;
+		}
+	}
+
+	printf("2022”N\n");
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+		std::cout << *itr << "\n";
+	}
 
 	return 0;
 }
